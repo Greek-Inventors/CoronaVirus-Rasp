f1252128fa49a9e41330801ee941a473fec04970
Greek Inventors <54319107+Greek-Inventors@users.noreply.github.com>
2020-03-28 19:49:31 +0200

    Add files via upload

--- coronavirus.py
+++ coronavirus.py
@@ -5,16 +5,39 @@ import time
 from bs4 import BeautifulSoup
 import requests
 import os
+import smtplib
+import socket
 
 mylcd = I2C_LCD_driver.lcd()
 
 # Insert your URL here
 url_c = "https://corona.help/country/Greece"
 
-infections = 0
+infections = "0"
 
 while True:
     try:
+
+        ''' email account and send '''
+        def alarmmail():
+            try:
+                check = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                to1 = 'emailtosend@gmail.com'
+                gmail_user = 'youremail@gmail.com'
+                gmail_pwd = 'yourpassword'
+                smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+                smtpserver.ehlo()
+                smtpserver.starttls()
+                smtpserver.ehlo()
+                smtpserver.login(gmail_user, gmail_pwd)
+                header = 'To:' + to1 + '\n' + 'From: ' + gmail_user + '\n' + 'Subject: Corona Virus - Covid-19 - ALARM \n'
+                msg = header + '\n Corona Virus Results ' + ' Infections: ' + str(infections_c) + ', Deaths: '+ str(deaths_c) + ', Survived: ' + str(survived_c) + '\n\n' + 'This is the Corona Virus Day Report From Raspberry System Virus Update' + '\n\n'
+                smtpserver.sendmail(gmail_user, to1, msg.encode("utf-8", errors="ignore"))
+                smtpserver.close()
+        
+            except socket.error as err:
+                pass
+        
         # Print to LCD
         mylcd.lcd_display_string("Time: %s" %time.strftime("  %H:%M"), 1)
         mylcd.lcd_display_string("Date: %s" %time.strftime("%d/%m/%Y"), 2)
@@ -32,14 +55,16 @@ while True:
         except:
             mylcd.lcd_clear()
 
+
         mylcd.lcd_clear()
 
         # Print LCD
         mylcd.lcd_display_string("Infect: " + infections_c, 1)
     
-        if (int(infections_c) > infections):
-            infections = int(infections_c)
+        if (str(infections_c) > infections):
+            infections = str(infections_c)
             os.system('mplayer -really-quiet sound.mp3 &')
+            alarmmail()
         
         mylcd.lcd_display_string("Deaths: " + deaths_c, 2)
         time.sleep(5)
@@ -48,6 +73,7 @@ while True:
         mylcd.lcd_display_string("Greece -- Update" ,2)
         time.sleep(5)
         mylcd.lcd_clear()
+        
     except:
         mylcd.lcd_clear()
     
